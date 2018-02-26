44205041f49cb0b67aefb3fd2aaec4c2eeba1d26
Chris Stoddart <chris.stoddart@dialexa.com>
2018-02-26 15:50:40 -0600

    added some rockin styling

--- src/routes/explore/index.js
+++ src/routes/explore/index.js
@@ -1,18 +1,22 @@
 import { h, app } from 'hyperapp';
 
+import './styles.css';
+
 const Explore = (state, actions) => () => (
-  <div>
-    <h1>Status Codes ({state.filter}{actions})</h1>
-    <input oninput={(event => actions.changeFilter(event))} autofocus />
-    {state.statusCodes
-    .filter((statusCode) => statusCode.code.startsWith(state.statusCodeFilter))
-    .map((statusCode) => (
-      <div>
-        <h3>{statusCode.code}</h3>
-        <h5>{statusCode.phrase}</h5>
-        <p>{statusCode.description}</p>
-      </div>
-    ))}
+  <div className="explore-container">
+    <div className="filter-container">
+      Filter <input oninput={(event => actions.changeFilter(event))} autofocus />
+    </div>
+    <div className="explore-codes-container">
+      {state.statusCodes
+      .filter((statusCode) => statusCode.code.startsWith(state.statusCodeFilter))
+      .map((statusCode) => (
+        <div>
+          <h3>{statusCode.code} - {statusCode.phrase}</h3>
+          <p>{statusCode.description}</p>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
